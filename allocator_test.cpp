1b989c99c56369b4329dace99c6983181b10a18c
Sergey Dundich <sergey.dundich@livestream.com>
2014-01-31 10:53:18 +0200

    Lots of improvements in allocator_test code.

--- allocator_test.cpp
+++ allocator_test.cpp
@@ -4,27 +4,31 @@
 #include <comutil.h>
 #include "DeckLinkAPI_h.h"
 
+//#define DISABLE_CUSTOM_ALLOCATOR
 BMDDisplayMode g_mode = bmdModePAL;
 
 class Alloc: public IDeckLinkMemoryAllocator
 {
-    LONG volatile ref_counter;
+    LONG volatile ref_count;
     LONG volatile buf_count;
+
+    Alloc(): ref_count(1), buf_count(0)  { printf("Alloc::Alloc (ref_count=1)\n"); }
+    ~Alloc()  { printf("Alloc::~Alloc current buf count %ld\n", buf_count); }
+
 public:
-    Alloc(): ref_counter(1), buf_count(0) {}
-    ~Alloc() { printf("Alloc::~Alloc current buf count %ld\n", buf_count); }
+    static IDeckLinkMemoryAllocator* CreateInstance()  { return new Alloc(); }
 
     virtual ULONG STDMETHODCALLTYPE AddRef()
     {
-        int cnt = InterlockedIncrement( &ref_counter );
-        printf("Alloc::AddRef (counter=%d)\n", cnt );
+        int cnt = InterlockedIncrement( &ref_count );
+        printf("Alloc::AddRef (ref_count=%d)\n", cnt );
         return cnt;
     }
 
     virtual ULONG STDMETHODCALLTYPE Release()
     {
-        int cnt = InterlockedDecrement( &ref_counter );
-        printf("Alloc::Release (counter=%d)\n", cnt );
+        int cnt = InterlockedDecrement( &ref_count );
+        printf("Alloc::Release (ref_count=%d)\n", cnt );
         if( cnt <= 0 )
             delete this;
         return cnt;
@@ -36,25 +40,25 @@ public:
         return E_NOINTERFACE;
     }
 
-    virtual HRESULT STDMETHODCALLTYPE AllocateBuffer( 
-        /* [in] */ unsigned long bufferSize,
-        /* [out] */ void **allocatedBuffer)
+    virtual HRESULT STDMETHODCALLTYPE AllocateBuffer( unsigned long bufferSize, void** pBuffer )
     {
         int cnt = InterlockedIncrement(&buf_count);
         //printf("Alloc::AllocateBuffer %08x (size %ld, total_count=%d)\n", (unsigned)*allocatedBuffer, bufferSize, cnt );
-        while(true)
+
+        for(;;)
         {
-            *allocatedBuffer = new char[bufferSize];
-            if( (uintptr_t)(*allocatedBuffer) % 16 == 0 )
+            *pBuffer = new char[bufferSize];
+
+            if( (uintptr_t)(*pBuffer) % 16 == 0 )
+            {
                 return S_OK;
-            delete[] *allocatedBuffer;
+            }
+
+            delete[] *pBuffer;
         }
-        
-        return S_OK;
     }
 
-    virtual HRESULT STDMETHODCALLTYPE ReleaseBuffer( 
-        /* [in] */ void *buffer)
+    virtual HRESULT STDMETHODCALLTYPE ReleaseBuffer( void* buffer )
     {
         int cnt = InterlockedDecrement(&buf_count);
         //printf("Alloc::DeallocateBuffer %08x (total_count=%d)\n", (int)buffer, cnt );
@@ -75,87 +79,113 @@ public:
     }
 };
 
-void iteration( IDeckLink* deckLink )
+void test_iteration( IDeckLink* deckLink, unsigned j )
 {
-    printf("Iteration started\n" );
+    printf("\nVideo+Audio Capture Testing Iteration #%u started!\n", j );
+
     IDeckLinkInput* input;
-    HRESULT result = deckLink->QueryInterface( IID_IDeckLinkInput, (void**)&input );
-    if( FAILED(result) )
+    printf("deckLink->QueryInterface(IID_IDeckLinkInput)...\n");
+    HRESULT hr = deckLink->QueryInterface( IID_IDeckLinkInput, (void**)&input );
+    if( FAILED(hr) )
     {
-        fprintf(stderr, "deckLink->QueryInterface failed\n");
+        fprintf(stderr, "deckLink->QueryInterface(IID_IDeckLinkInput) failed\n");
         return;
     }
 
-    Alloc* alloc = new Alloc();
+#ifndef DISABLE_CUSTOM_ALLOCATOR
+    IDeckLinkMemoryAllocator* alloc = Alloc::CreateInstance();
     printf("input->SetVideoInputFrameMemoryAllocator...\n");
-    result = input->SetVideoInputFrameMemoryAllocator( alloc );
+    hr = input->SetVideoInputFrameMemoryAllocator(alloc);
     alloc->Release();
-    if( FAILED(result) )
+    if( FAILED(hr) )
     {
-        input->Release();
-        fprintf(stderr, "input->SetVideoInputFrameMemoryAllocator failed\n");
-        return;
+        fprintf(stderr, "input->SetVideoInputFrameMemoryAllocator(obj) failed\n");
     }
-
-    printf("input->EnableVideoInput...\n");
-    result = input->EnableVideoInput( g_mode, bmdFormat8BitYUV, 0 );
-    if( FAILED(result) )
+    else
     {
-        input->Release();
-        fprintf(stderr, "input->EnableVideoInput failed\n");
-        return;
-    }
+#endif
+        printf("input->EnableVideoInput...\n");
+        hr = input->EnableVideoInput( g_mode, bmdFormat8BitYUV, 0 );
+        if( FAILED(hr) )
+        {
+            fprintf(stderr, "input->EnableVideoInput failed\n");
+        }
+        else
+        {
+            printf("input->EnableAudioInput...\n");
+            hr = input->EnableAudioInput( bmdAudioSampleRate48kHz, bmdAudioSampleType32bitInteger, 16 );
+            if( FAILED(hr) )
+            {
+                fprintf(stderr, "input->EnableAudioInput failed\n");
+            }
+            else
+            {
+                printf("input->StartStreams...\n");
+                hr = input->StartStreams();
+                if( FAILED(hr) )
+                {
+                    fprintf(stderr, "input->StartStreams failed\n");
+                }
+                else
+                {
+                    printf( "Video+Audio Capture working 5 sec...\n");
+                    Sleep(5*1000);
 
-    printf("input->EnableAudioInput...\n");
-    result = input->EnableAudioInput( bmdAudioSampleRate48kHz, bmdAudioSampleType32bitInteger, 16 );
-    if( FAILED(result) )
-    {
-        input->Release();
-        fprintf(stderr, "input->EnableAudioInput failed\n");
-        return;
-    }
+                    printf("input->StopStreams...\n");
+                    hr = input->StopStreams();
+                    if( FAILED(hr) )
+                    {
+                        fprintf(stderr, "input->StopStreams failed\n");
+                    }
+                }
 
-    printf("input->StartStreams...\n");
-    result = input->StartStreams();
-    if( FAILED(result) )
-    {
-        input->Release();
-        fprintf(stderr, "input->StartStreams failed\n");
-        return;
-    }
+                printf("input->DisableAudioInput...\n");
+                hr = input->DisableAudioInput();
+                if( FAILED(hr) )
+                {
+                    fprintf(stderr, "input->DisableAudioInput failed\n");
+                }
+            }
 
-    printf( "Started. working 5 sec\n");
-    Sleep(5 * 1000);
-    printf("input->StopStreams...\n");
-    result = input->StopStreams();
-    if( FAILED(result) )
-    {
-        input->Release();
-        fprintf(stderr, "input->StopStreams failed\n");
-        return;
-    }
+            printf("input->DisableVideoInput...\n");
+            hr = input->DisableVideoInput();
+            if( FAILED(hr) )
+            {
+                fprintf(stderr, "input->DisableVideoInput failed\n");
+            }
+        }
 
-    printf("input->DisableAudioInput...\n");
-    input->DisableAudioInput();
-    printf("input->DisableVideoInput...\n");
-    input->DisableVideoInput();
+#ifndef DISABLE_CUSTOM_ALLOCATOR
+        hr = input->SetVideoInputFrameMemoryAllocator(NULL);
+        if( FAILED(hr) )
+        {
+            fprintf(stderr, "input->SetVideoInputFrameMemoryAllocator(NULL) failed\n");
+        }
+    }
+#endif
+
     printf("input->Release...\n");
     input->Release();
-    printf("Stopped. some memory operations for tests\n");
 
-    int global_tests = 0;
-    for( int i = 0; i < 50; ++i )
+    printf( "Video+Audio Stopped.\n"
+            "Allocating+Reading+Deallocating 50 memory buffers of 1920x1080x4 bytes each.\n" );
+
+    unsigned memory_sum = 0;
+
+    for( unsigned k = 0; k < 50; ++k )
     {
-        int* buf = new int[ 1920 * 1080 ];
-        int sum = 0;
-        for( int i = 0; i < 1920 * 1080; ++i )
-            sum += buf[i];
-        global_tests += sum;
+        // memory allocating+reading+deallocating iteration
+        unsigned* buf = new unsigned[1920*1080];
+
+        for( size_t j = 0; j < 1920*1080; ++j )
+        {
+            memory_sum += buf[j];
+        }
+
         delete[] buf;
     }
-    printf("Iteration finished successful (sum=%d)\n", global_tests );
 
-    printf("\n");
+    printf("Video+Audio Capture Testing Iteration #%u finished (memory_sum=0x%x)!\n\n", j, memory_sum );
 }
 
 int main( int argc, char* argv[] )
@@ -163,29 +193,30 @@ int main( int argc, char* argv[] )
     IDeckLinkIterator*  deckLinkIterator;
     IDeckLink*  deckLink;
     int  numDevices = 0;
-    HRESULT  result;
+    HRESULT  hr;
 
     //  Initialize COM on this thread
-    result = CoInitialize(NULL);
-    if (FAILED(result))
+    hr = CoInitialize(NULL);
+    if (FAILED(hr))
     {
-        fprintf(stderr, "Initialization of COM failed - result = %08x.\n", result);
+        fprintf(stderr, "Initialization of COM failed - hr = %08x.\n", hr);
         return 1;
     }
 
     // Create an IDeckLinkIterator object to enumerate all DeckLink cards in the system
-    result = CoCreateInstance(CLSID_CDeckLinkIterator, NULL, CLSCTX_ALL, IID_IDeckLinkIterator, (void**)&deckLinkIterator);
-    if (FAILED(result))
+    hr = CoCreateInstance(CLSID_CDeckLinkIterator, NULL, CLSCTX_ALL, IID_IDeckLinkIterator, (void**)&deckLinkIterator);
+    if (FAILED(hr))
     {
         fprintf(stderr, "A DeckLink iterator could not be created.  The DeckLink drivers may not be installed.\n");
+        CoUninitialize();
         return 1;
     }
 
     {
         // We can get the version of the API like this:
         IDeckLinkAPIInformation*	deckLinkAPIInformation;
-        result = deckLinkIterator->QueryInterface(IID_IDeckLinkAPIInformation, (void**)&deckLinkAPIInformation);
-        if (result == S_OK)
+        hr = deckLinkIterator->QueryInterface(IID_IDeckLinkAPIInformation, (void**)&deckLinkAPIInformation);
+        if (hr == S_OK)
         {
             LONGLONG  deckLinkVersion;
             int  dlVerMajor, dlVerMinor, dlVerPoint;
@@ -197,29 +228,29 @@ int main( int argc, char* argv[] )
             dlVerMinor = (deckLinkVersion & 0x00FF0000) >> 16;
             dlVerPoint = (deckLinkVersion & 0x0000FF00) >> 8;
 
-            printf("DeckLinkAPI version: %d.%d.%d\n", dlVerMajor, dlVerMinor, dlVerPoint);
+            printf("DeckLink API version: %d.%d.%d\n", dlVerMajor, dlVerMinor, dlVerPoint);
 
             deckLinkAPIInformation->Release();
         }
     }
 
-    result = deckLinkIterator->Next(&deckLink); // first device
-    if( FAILED(result) )
+    hr = deckLinkIterator->Next(&deckLink); // first device
+    if( FAILED(hr) )
     {
         fprintf(stderr, "No Blackmagic Design devices were found.\n");
         deckLinkIterator->Release();
+        CoUninitialize();
         return 1;
     }
 
-    for( int i = 0; i < 50; ++i )
-        iteration(deckLink);
+    for( unsigned j = 0; j < 50; ++j )
+    {
+        test_iteration( deckLink, j+1 );
+    }
 
     deckLink->Release();
     deckLinkIterator->Release();
-
-    // Uninitalize COM on this thread
     CoUninitialize();
-
     return 0;
 }
 
